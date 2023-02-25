15744ad26e3fddd1f339cadfd338dfd291739503
olardaniil <olardaniil@yandex.ru>
2023-02-25 21:55:54 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    
    Initial commit
    
    Changes to be committed:
    	new file:   app.py
    	new file:   db.py
    	new file:   urls.py

---
+++ db.py
@@ -0,0 +1,48 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+def create_connection():
+    connection = None
+
+    try:
+        connection = mysql.connector.connect(
+            host = '95.163.241.100',
+            port = 3306,
+            user = 'super_user', 
+            password = '****9963AAdd',
+            database = 'vkurse_database')
+
+        print("Подключение к базе данных MySQL прошло успешно")
+        return connection
+
+    except Error as e:
+        print(f"Произошла ошибка в create_connection'{e}'")
+        return connection
+
+def сheck(connection):
+    with connection.cursor() as cursor:
+        try:
+            cursor.execute("SELECT `phone_number` FROM `phone_number_verification_codes` WHERE `id` = 1")
+            result = cursor.fetchall()
+
+            if len(result) != 0:
+                return result[0][0]
+            else:
+                return 'Noresult'
+        
+        except Error as e:
+            print(f"Произошла ошибка сheck_user_block'{e}'")
+            return e
+        
+
+def save_phone_number(connection, phome_number_value):
+    with connection.cursor() as cursor:
+        try:
+            code = 12345
+            cursor.executemany("INSERT INTO `phone_number_verification_codes` (`id`, `phone_number`, `verification_code`, `dt_create`) VALUES (NULL, %s, %s, NOW())", [(str(phome_number_value), str(code))])
+            # connection.commit()
+            return 'successful'
+        
+        except Error as e:
+            return e
---
+++ urls.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, request, json, jsonify
+from flask import Flask
+from db import *
+
+application = Flask(__name__)
+
+
+@application.route("/")
+def hello():
+   return "<h1>Hello!</h1>"
+
+@application.route('/sendphonenumber', methods=["GET", "POST"])
+def sendphonenumber():
+
+    if request.method =="POST":
+
+        phone_number = request.form["phone_number"]
+
+        result = save_phone_number(create_connection(), phone_number)
+
+        if result == 'successful':
+            return jsonify(["successful"])
