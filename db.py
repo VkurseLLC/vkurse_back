be1a8e75af04d73bf6f8d4e7531cde773bf5cd42
seemyown <semyon.albeev@gmail.com>
2023-03-05 23:48:09 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   db.py
    	encode_decode.py

--- db.py
+++ db.py
@@ -2,6 +2,7 @@ import mysql.connector
 from mysql.connector import Error
 from bot_config import *
 import hashlib
+from encode_decode import *
 
 # ---------------------------------------------------------------------------------- #
 
@@ -35,14 +36,15 @@ def user_authorisation(connection, phome_number_value, verification_code_value):
             phome_number_value = phome_number_value.replace(')','')
             phome_number_value = phome_number_value.replace('-','')
             phome_number_value = phome_number_value.replace(' ','')
-            
 
             phome_number_value  = (hashlib.sha256(repr(phome_number_value).encode())).hexdigest()
-
+            # phome_number_value = encrypt(repr(phome_number_value), crypto_password)
             verification_code_value  = (hashlib.sha256(repr(int(verification_code_value)).encode())).hexdigest()
-   
+            # verification_code_value = encrypt(repr(verification_code_value), crypto_password)
+
             cursor.execute("SELECT `id` FROM `phone_number_verification_codes` WHERE `phone_number` = %s AND verification_code = %s", (str(phome_number_value), str(verification_code_value)))
             result = cursor.fetchall()
+            print(result)
 
             if len(result) != 0: 
 
@@ -50,16 +52,7 @@ def user_authorisation(connection, phome_number_value, verification_code_value):
                 result = cursor.fetchall()
 
                 if len(result) != 0:
-                    cursor.execute("SELECT `id` FROM `users_account_data` WHERE `users_id` = %s", (int(result[0][0]),))
-                    user_account_status = cursor.fetchall()
-
-                    if len(user_account_status) != 0:
-
-                        return ['successful', result[0][0], 'old_user']
-                    
-                    else:
-
-                        return ['successful', result[0][0], 'new_user']
+                    return ['successful', result[0][0]]
                 
                 else:
                     cursor.executemany("INSERT INTO users (id, phone_number, dt_reg) VALUES (NULL, %s, NOW())", [(str(phome_number_value), )])
@@ -68,16 +61,7 @@ def user_authorisation(connection, phome_number_value, verification_code_value):
                     cursor.execute("SELECT `id` FROM `users` WHERE `phone_number` = %s", (str(phome_number_value),))
                     result = cursor.fetchall()
 
-                    cursor.execute("SELECT `id` FROM `users_account_data` WHERE `users_id` = %s", (int(result[0][0]),))
-                    user_account_status = cursor.fetchall()
-
-                    if len(user_account_status) != 0:
-
-                        return ['successful', result[0][0], 'old_user']
-                    
-                    else:
-
-                        return ['successful', result[0][0], 'new_user']
+                    return ['successful', result[0][0]]
 
             else:
                 return ['verification_code_not_found']
@@ -91,7 +75,6 @@ def check_username_availability(connection, username_value):
     with connection.cursor() as cursor:
         try:
             username_value = username_value.replace(' ','')
-            username_value.lower()
             
             # Делаем запрос на проверку занятости username
             cursor.execute("SELECT `users_id`, `username` FROM `users_account_data` WHERE `username` = %s ORDER BY `dt_upd` DESC", (str(username_value),))
@@ -119,8 +102,15 @@ def check_username_availability(connection, username_value):
         
 def filling_profile(connection, users_id, username, first_name, d_birth, city):
     with connection.cursor() as cursor:
+
         try:
             check_username = check_username_availability(create_connection(), username)
+
+            # users_id = encrypt(repr(users_id), crypto_password) # Тип колонки в БД int, невозможно вставить строку
+            # username = encrypt(repr(username), crypto_password)
+            # first_name = encrypt(repr(first_name), crypto_password)
+            # d_birth = encrypt(repr(d_birth), crypto_password) # Тип колонки в БД date, невозможно вставить строку
+            city = encrypt(repr(city), crypto_password)
 
             if check_username[0] == 'True':
                 cursor.executemany("INSERT INTO users_account_data (id, users_id, username, first_name, d_birth, city, dt_upd) VALUES (NULL, %s, %s, %s, %s, %s, NOW())",
@@ -160,4 +150,6 @@ def template(connection):
 
 # print(user_authorisation(create_connection(), '+7 (928) 753-90-56', 97173))
 
-# print(filling_profile(create_connection(), "2", "seemyown", "Semyon", "2003-07-03", "Rostov-on-Don"))
+# print(filling_profile(create_connection(), "2", "seemyowns", "Semyon", "2003-07-03", "Rostov-on-Don")) # Заполнение профиля
+
+# print(user_authorisation(create_connection(), '79958932523', 80252)) # Прооверка авторизации пользователя
